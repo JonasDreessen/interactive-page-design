230e902b35fdd4764fb3dd8d49a9ac3ebfc7c577
JonasDreessen <dreessenjonas95@gmail.com>
2019-08-01 13:54:00 +0200

    add counter function

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -29,12 +29,54 @@ function changeImageSourceLeftClick(){
     }
     }
 
-function makeNumberCountUp(){
+var numberCounterPeopleComing = 0;
+function makeNumberCountUpPeopleComing(){
+    numberCounterPeopleComing += 10;
+    if(numberCounterPeopleComing < 900){
     let peopleComing = document.querySelector(".people-coming");
-    for (let i = 0; i < 800; i++){
-        peopleComing.innerHTML = i;
+    peopleComing.innerHTML = numberCounterPeopleComing;
+    } else{
+        clearInterval(makeNumberCountUpPeopleComing);
+    }
+};
+
+var numberCountUpArtistPlaying = 0;
+function makeNumberCountUpArtistPlaying(){
+    numberCountUpArtistPlaying += 1;
+    if(numberCountUpArtistPlaying < 8){
+    let ArtistPlaying = document.querySelector(".artist-playing");
+    ArtistPlaying.innerHTML = numberCountUpArtistPlaying;
+    } else{
+        clearInterval(makeNumberCountUpArtistPlaying);
+    }
+};
+
+var numberCountUpDuration = 0;
+function makeNumberCountUpDuration(){
+    numberCountUpDuration += 1;
+    if(numberCountUpDuration < 7){
+    let duration = document.querySelector(".duration");
+    duration.innerHTML = numberCountUpDuration + ' hours';
+    } else{
+        clearInterval(makeNumberCountUpDuration);
+    }
+};
+
+var numberCountUpTemperature = 0;
+function makeNumberCountUpTemperature(){
+    numberCountUpTemperature += 1;
+    if(numberCountUpTemperature < 19){
+    let temperature = document.querySelector(".temperature");
+    temperature.innerHTML = numberCountUpTemperature + ' degrees';
+    } else{
+        clearInterval(makeNumberCountUpTemperature);
     }
 }
 
-makeNumberCountUp();
+
+var makeNumberCountUpPeopleComing = setInterval(makeNumberCountUpPeopleComing, 15);
+var makeNumberCountUpArtistPlaying = setInterval(makeNumberCountUpArtistPlaying, 150);
+var makeNumberCountUpDuration = setInterval(makeNumberCountUpDuration, 160);
+var makeNumberCountUpTemperature = setInterval(makeNumberCountUpTemperature, 100);
+
 
