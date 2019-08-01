6a3d145bfec2206b42a1e2b94c8e3e864c3b5788
JonasDreessen <dreessenjonas95@gmail.com>
2019-08-01 11:24:35 +0200

    add picture and javascript

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,7 +1,20 @@
+var clickCounter = 0;
+let imageSourceArray = ["./assets/images/flying-pig-grote-markt.jpg", "./assets/images/concert-picture.jpg","./assets/images/party-picture.jpg"];
 
 
-function changeImageSource(){
+
+//document.querySelector(".left").addEventListener("click",changeImageSource);
+document.querySelector(".right").addEventListener("click",changeImageSourceRightClick);
+
+function changeImageSourceRightClick(){
+clickCounter ++;
 let headerImage = document.querySelector(".header-image");
-headerImage.src = "hackanm.gif";
+
+if (clickCounter < imageSourceArray.length){
+    headerImage.src = imageSourceArray[clickCounter];
+} else {
+    clickCounter = 0;
+    headerImage.src = imageSourceArray[clickCounter];
+}
 }
 
